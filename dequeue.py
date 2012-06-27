cc001f4d9f347dc0be70c8946d0a653d915c7d04
Phil! Gold <phil_g@pobox.com>
2012-06-27 10:05:19 -0400

    New dequeuing strategy that always focuses on high-zoom tiles first.

--- dequeue.py
+++ dequeue.py
@@ -54,6 +54,54 @@ class DequeueByPctStrategy:
         else:
             return self.getMessage()
 
+class DequeueByFixedPctStrategy:
+    """Basically like DequeueByPctStrategy, but the target percentages are
+    derived from the total number of tiles at each zoom level, not the
+    current queue lengths."""
+
+    def __init__(self, thread_number, maxz, amqp_channel):
+        self.thread_number = thread_number
+        self.maxz = maxz
+        self.chan = amqp_channel
+        self.render_count = [ 0 for z in range(0, maxz + 1) ]
+        self.render_time = [ 0 for z in range(0, maxz + 1) ]
+        self.queue_pcts = [ 2**z / float(2**(maxz+1) - 1) for z in range(0, maxz + 1) ]
+        self.chan.basic_recover(requeue=True)
+
+    def recordRender(self, zoom, time):
+        self.render_count[zoom] += 1
+        self.render_time[zoom] += time
+        
+    def getMessage(self):
+        queues = [ self.chan.queue_declare(queue='toposm_z{0}'.format(z), passive=True)[1] for z in range(0, self.maxz + 1) ]
+        render_sum = sum([ c for z, c in enumerate(self.render_count) if queues[z] > 0 ])
+        if render_sum == 0:
+            render_sum = 1
+        render_pcts = [ float(c) / render_sum for c in self.render_count ]
+        # Seed choice with the last queue that has stuff in it.
+        for z in range(0, self.maxz + 1):
+            if queues[z] > 0:
+                chosen_queue = z
+        scale = 2**(self.maxz - chosen_queue)
+        pct_diff = self.queue_pcts[chosen_queue] * scale - render_pcts[chosen_queue]
+        # See if there's a more-neglected queue.
+        for z in range(0, chosen_queue):
+            if queues[z] > 0 and self.queue_pcts[z] * scale - render_pcts[z] > pct_diff:
+                chosen_queue = z
+                pct_diff = self.queue_pcts[z] * scale - render_pcts[z]
+        console.printMessage('q%: ' + ' '.join([ '%.2f' % (n * scale * 100) for n in self.queue_pcts ]))
+        console.printMessage('r%: ' + ' '.join([ '%.2f' % (n * 100) for n in render_pcts ]))
+        console.printMessage('c: ' + ' '.join([ '%d:%d' % (i, self.render_count[i]) for i in range(len(self.render_count)) ]))
+        console.printMessage('t/c: ' + ' '.join([ '%d:%.1f' % (i, self.render_time[i] / self.render_count[i] if self.render_count[i] > 0 else 0) for i in range(len(self.render_time)) ]))
+        # Now that we know which queue is most in need of rendering, do it.  If
+        # it's empty (possible, since someone else could have taken the last
+        # message while we were thinking), just try again.
+        msg = self.chan.basic_get('toposm_z{0}'.format(chosen_queue))
+        if msg:
+            return msg
+        else:
+            return self.getMessage()
+
 class DequeueByZoomStrategy:
     def __init__(self, thread_number, maxz, amqp_channel):
         self.thread_number = thread_number
